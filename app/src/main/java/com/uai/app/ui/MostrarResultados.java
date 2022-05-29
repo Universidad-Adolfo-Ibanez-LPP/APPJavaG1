754ce9a5912fbc4cbdbbee51928544b983867d88
VicenteGaray <100960324+VicenteGaray@users.noreply.github.com>
2022-05-29 12:47:37 -0400

    Update MostrarResultados.java

--- app/src/main/java/com/uai/app/ui/MostrarResultados.java
+++ app/src/main/java/com/uai/app/ui/MostrarResultados.java
@@ -10,18 +10,33 @@ import javax.swing.table.TableModel;
 import java.awt.*;
 import java.util.HashSet;
 
+/*
+Creacion de la clase Mostrar Resultados, la cual es usada en
+la clase Buscar Datos
+
+Esta clase tiene como objetivo mostrar unica y exclusivamente los libros
+que resultaron de la busqueda.
+*/
+
 public class MostrarResultados extends JFrame {
+    /*
+    Creacion de los distintos elementos que posee el JFrame
+    de Mostrar Resultados.
+    */
     private JPanel mainPanel;
     private JLabel encabezado;
     private JPanel mainTableConatiner;
 
+    //Constructor de la clase Mostrar Resultados, y que recibe como parametro una matriz de Strings.
     public MostrarResultados(String[][] resultado) {
 
         this.mainPanel= mainPanel;
         this.setContentPane(mainPanel);
 
+
         String[] titles = {"titulo", "autor", "anio", "estante_numero", "estante_seccion", "piso", "edificio", "sede"};
-        String[][] dataTabla = resultado;
+        String[][] dataTabla = resultado;/*inicializacion de la variable dataTabla, a partir de resultado, la cual
+        es una matriz de Strings que contiene los resultados de la busqueda que se obtuvieron en la clase BuscarDatosUI*/
 
 
         TableModel tableModel = new DefaultTableModel(dataTabla, titles);
