2f2d7558abf2636e215e67ce81d3d01850a62b16
Andrés Guerra <100784908+AndrewWar29@users.noreply.github.com>
2022-05-29 10:03:59 -0400

    Base App 2

---
+++ app/src/main/java/com/uai/app/ui/utils/UAIJFrame.java
@@ -0,0 +1,30 @@
+package com.uai.app.ui.utils;
+
+import com.uai.app.exceptions.NotMainPanelLoadedException;
+
+import javax.swing.*;
+
+public class UAIJFrame extends JFrame {
+
+    private JPanel mainPanel;
+
+    public UAIJFrame(String title) {
+        super(title);
+    }
+
+    public void setMainPanel(JPanel mainPanel) {
+        this.mainPanel = mainPanel;
+        this.setContentPane(mainPanel);
+    }
+
+    public void showUI() throws NotMainPanelLoadedException {
+        if (AppUtils.isNull(mainPanel)){
+            throw new NotMainPanelLoadedException();
+        }
+        this.setSize(500,500);
+        this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        //this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+}
